008b3bebfdd3b4bb6adc77084ba2defdf4832720
BitElves <kynatmansha03@gmail.com>
2024-08-02 01:00:28 +0500

    final commit

--- chatbot/dmv_chatbot/src/Chatbot/ChatInterface.jsx
+++ chatbot/dmv_chatbot/src/Chatbot/ChatInterface.jsx
@@ -183,7 +183,7 @@ const ChatInterface = () => {
           {/* User profile */}
           <div className="flex flex-col items-center bg-indigo-100 dark:bg-indigo-900 border border-gray-200 dark:border-gray-700 mt-4 w-full py-6 px-4 rounded-lg">
             <div className="h-20 w-20 rounded-full border overflow-hidden">
-              <img src="https://avatars3.githubusercontent.com/u/2763884?s=128" alt="Avatar" className="h-full w-full" />
+              <img src="/src/Chatbot/bot.png" alt="Avatar" className="h-full w-full" />
             </div>
             <div className="text-sm font-semibold mt-2">{JSON.parse(localStorage.getItem('user')).email}</div>
             <div className="text-xs text-gray-500 dark:text-gray-400">Chatbot User</div>
